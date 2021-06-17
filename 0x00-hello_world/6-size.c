a61ad08c8c1a7d0f1a7a3b08b08a8ce6b952c3b3
HenryIyke <realhenryiyke@gmail.com>
2021-06-17 11:55:55 -0700

    Intel assembly

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char c;
+ int i;
+  long int r;
+  long long int l;
+  float f;
+  
+  printf("Size of a char: %lu.\n" (unsigned long)sizeof(c));
+  return (0);
+}
