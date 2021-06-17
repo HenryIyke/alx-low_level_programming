ff0c7e32e9743036e697b06b80d93cc375f789c1
HenryIyke <realhenryiyke@gmail.com>
2021-06-17 10:25:35 -0700

    Put C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point for print code
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
