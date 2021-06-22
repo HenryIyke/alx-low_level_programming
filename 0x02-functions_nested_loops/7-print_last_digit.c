44e16dd92a04b8ce60f5ae24b81ecd409df73755
HenryIyke <realhenryiyke@gmail.com>
2021-06-22 20:58:25 +0100

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,25 @@
-#include <stdio.h>
 #include "holberton.h"
-
 /**
- * print_last_digit - check the code for ALX School students.
- * @n: int for the argument of the function
- * Return: Always 0.
+ * _abs - returns -n if n is less than 0 otherwise n
+ * @n: integer to be tested
+ * Return: _abs(n)
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-1 * n);
+}
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer whose last digit is to be printed
+ * Return: lastdigit
  */
 int print_last_digit(int n)
 {
-if (n < 0)
-n = n * -1;
-_putchar (n % 10 + '0');
-return (n % 10);
+int lastDigit;
+lastDigit = _abs((n % 10));
+_putchar(lastDigit + 48);
+return (lastDigit);
 }
