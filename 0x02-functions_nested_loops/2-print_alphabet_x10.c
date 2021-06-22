d82c38c86dc962e8de1b9b2be8bbaf87a7048c83
HenryIyke <realhenryiyke@gmail.com>
2021-06-22 12:10:10 +0100

    Print multiple lines of alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Entry point for nested loop
+ *
+ * Return: Always 0.
+ */
+int print_alphabets_x10(void)
+{
+int i = 0;
+for (i = 0; i <= 10; i++)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+puchar(ch);
+}
+putchar('\n');
+}
+}
