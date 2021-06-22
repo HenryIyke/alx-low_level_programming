9961c7f7fb2aff88c6fe1abd91c845613de08ddd
HenryIyke <realhenryiyke@gmail.com>
2021-06-22 12:16:41 +0100

    Print multiple lines of alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0.
  */
-int print_alphabets_x10(void)
+int print_alphabet_x10(void)
 {
 int i = 0;
 for (i = 0; i <= 10; i++)
@@ -14,8 +14,10 @@ for (i = 0; i <= 10; i++)
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-_puchar(ch);
+_putchar(ch);
 }
 _putchar('\n');
+}
+return (0);
 }
 
