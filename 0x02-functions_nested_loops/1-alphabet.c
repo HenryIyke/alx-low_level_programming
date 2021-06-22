79e78dfaf1ddd91c318a5b1f61a5b42c0da93e9f
HenryIyke <realhenryiyke@gmail.com>
2021-06-22 11:56:43 +0100

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char c = 0;
+for (c = 'a'; c <= 'z'; c++)
+{
+ _putchar(c);
+}
+ _putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,5 +2,7 @@
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
+void print_alphabet(void);
+
 
 #endif
