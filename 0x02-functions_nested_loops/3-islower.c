3a538150ab975d22e572e4ced363fd43855396c7
HenryIyke <realhenryiyke@gmail.com>
2021-06-22 13:14:56 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 
 /**
  * _islower - check the code for ALX School students.
- *
+ *@c: parameter the int will use to check for the argument of the function
  * Return: Always 0.
  */
 int _islower(int c);
