7a8d17e1bd0695bce049eb1e29a976ef719f0096
HenryIyke <realhenryiyke@gmail.com>
2021-06-24 19:38:10 +0100

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,14 @@ int c;
 char f[] = "Fizz";
 char b[] = "Buzz";
 char fb[] = "FizzBuzz";
+
 for (c = 1; c <= 100; c++)
 {
-if (c % 3 == 0 && c % 5 == 0)
+if (c == 100)
+{
+printf("%s", b);
+}
+else if (c % 3 == 0 && c % 5 == 0)
 {
 printf("%s ", fb);
 }
