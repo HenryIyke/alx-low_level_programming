90c9d771a7451fa3a347fcd4c063e5f24aee206d
HenryIyke <realhenryiyke@gmail.com>
2021-06-24 16:29:23 +0100

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x03-debugging/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_diagonal - code to print for digits
+ * @n: Int for argument of the function
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n >= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
