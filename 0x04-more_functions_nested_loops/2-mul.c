93a60dae7d6257de6273eb3599bb41cc8ff8fcd8
HenryIyke <realhenryiyke@gmail.com>
2021-06-24 10:42:56 +0100

    multiplication of two variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * mul - Multiplication of two variables.
+ *@a: parameter the int will use to check for the argument of the function
+ *@b: parameter the int will use to check for the argument of the function
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
