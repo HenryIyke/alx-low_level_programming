30a03f6a99fd893d9393ef7ff4266880eb8d2e6b
HenryIyke <realhenryiyke@gmail.com>
2021-06-24 19:18:14 +0100

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,22 @@
 int main(void)
 {
 int c;
-for (c = 1; c<= 100; c++)
+for (c = 1; c <= 100; c++)
 {
-if (c % 3 = 0)
+if (c % 3 == 0 && c % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (c % 3 == 0)
 {
 printf("Fizz ");
 }
-else if (c % 5 = 0)
+else if (c % 5 == 0)
 {
 printf("Buzz ");
-}
-else if (c % 3 = 0 && c % 5 = 0)
-{
-printf("FizzBuzz ");
 }
 else
 printf("%d ", c);
 }
+return (0);
 }
