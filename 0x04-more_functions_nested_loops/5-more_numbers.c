a651496987ebf17efb6ac0dc7495e39d839d3022
HenryIyke <realhenryiyke@gmail.com>
2021-06-24 13:54:59 +0100

    Print more numbers again

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * more_numbers - code to print for digits
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int i = 0;
+int c = 0;
+
+while (i <= 9)
+{
+while (c <= 14)
+{
+if (c > 9)
+{
+putchar(c / 10 + '0');
+putchar(c % 10 + '0');
+c++
+}
+}
+}
+putchar('\n');
+}
