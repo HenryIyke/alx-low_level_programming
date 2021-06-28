e01941db279937dae160683690faa39296cfe245
HenryIyke <realhenryiyke@gmail.com>
2021-06-28 18:07:07 +0100

    Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- * puts_half - Code to print string to standard output
- * @str: string value for the argument of the function
- * Return: void
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: If odd number of chars, print (length - 1) / 2
  */
-
 void puts_half(char *str)
 {
-int c;
-int a;
+int i;
 
-for (c = 0; str[c] != 0; c++)
+for (i = 0; str[i] != '\0'; i++)
+i++;
+for (i /= 2; str[i] != '\0'; i++)
 {
-}
-if (c % 2 == 0)
-{
-for ((a = c / 2); str[a] != 0; a++)
-{
-_putchar(str[a]);
-}
-}
-else
-{
-for ((a = (c - 1) / 2); str[a] != '\0'; a++)
-{
-_putchar(str[a]);
-}
+_putchar(str[i]);
 }
 _putchar('\n');
 }
