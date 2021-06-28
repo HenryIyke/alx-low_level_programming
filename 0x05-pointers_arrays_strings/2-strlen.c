f4bd7ac5e4ad02d838b047f81634c1b20ad81962
HenryIyke <realhenryiyke@gmail.com>
2021-06-28 01:53:34 +0100

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 /**
  * _strlen - Code to print the length of a string
- * @c: Int value for the argument of the function
+ * @s: Int value for the argument of the function
  * Return: Always 0
  */
 
 int _strlen(char *s)
 {
-  int c;
+int c;
 
 for (c = 0; s[c] != 0; c++)
 {
 }
-return(c);
+return (c);
 }
