2892bc7ac25c6c25525d2657524267b26d6e174e
HenryIyke <realhenryiyke@gmail.com>
2021-06-28 15:49:01 +0100

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - Code to print string to standard output
+ * @str: string value for the argument of the function
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int c;
+int a;
+
+for (c = 0; str[c] != 0; c++)
+{
+}
+if (c / 2 == 0)
+{
+for (a = c / 2; str[a] != 0; a++)
+{
+_putchar(str[a]);
+}
+}
+else
+{
+int length;
+length = (c - 1) / 2;
+for (a = length; str[a] != 0; a++)
+{
+_putchar(str[a]);
+}
+}
+_putchar('\n');
+}
