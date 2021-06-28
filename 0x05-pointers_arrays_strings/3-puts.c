4c58a9f7a9442a3c31f955e19edb3b58c0017315
HenryIyke <realhenryiyke@gmail.com>
2021-06-28 02:03:39 +0100

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - Code to print string to standard output
+ * @n: Int value for the argument of the function
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+_puts(*str)
+}
