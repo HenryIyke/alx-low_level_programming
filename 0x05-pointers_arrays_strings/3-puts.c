cd905fc90c37f952a62db290108c7f13b5aa8f8e
HenryIyke <realhenryiyke@gmail.com>
2021-06-28 02:06:52 +0100

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 /**
  * _puts - Code to print string to standard output
- * @n: Int value for the argument of the function
+ * @str: string value for the argument of the function
  * Return: void
  */
 
 void _puts(char *str)
 {
-_puts(*str)
+_puts(str);
 }
