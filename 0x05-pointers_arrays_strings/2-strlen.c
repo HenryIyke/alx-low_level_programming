77f5950d9bd3c66931f18e73216f8b6a18c2f5ae
HenryIyke <realhenryiyke@gmail.com>
2021-06-28 01:46:20 +0100

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - Code to print the length of a string
+ * @c: Int value for the argument of the function
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+return(_strlen)
+}
