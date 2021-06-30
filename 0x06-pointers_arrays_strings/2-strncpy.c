d3809a56f710c76a6694b697b060410bdfbd8364
HenryIyke <realhenryiyke@gmail.com>
2021-06-30 23:59:24 +0100

    Third commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -24,6 +24,6 @@ break;
 }
 }
 while (b != n)
-dest[++b] = '\0';
+dest[b++] = '\0';
 return (dest);
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,23 @@
 #include <stdio.h>
 /**
  * string_toupper - Code to return upper cases of a lowercase array
- * @a: int value for the argument of the function
- * @n: int value for the argument of the function
+ * @s: int value for the argument of the function
+ * int value for the argument of the function
  *
  * Return: Void
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+int i = 0;
+
+while (s[1] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+i++;
+}
+return (0);
+}
