b9fe2e75f89ab7fd99f9cf17bec0b0c3e4004690
HenryIyke <realhenryiyke@gmail.com>
2021-07-01 12:59:30 +0100

    Third commit

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -8,6 +8,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *_strncpy(char *dest, char *src, int n);
+char *cap_string(char *);
+
 
 
 #endif /* HOLBERTON_H */
