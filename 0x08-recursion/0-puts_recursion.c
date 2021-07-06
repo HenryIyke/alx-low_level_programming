8d5815521153d3b3cf7eab75e4b4c747adb61aca
HenryIyke <realhenryiyke@gmail.com>
2021-07-06 23:07:51 +0100

    First recursions commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: String argument
+ *
+ *
+ * Return: pointer to s
+ */
+
+void _puts_recursion(char *s)
+{
+_putchar(s);
+_putchar('\n');
+}
