abc517f8002ca279eeebb04a8dc28791a4b0218c
HenryIyke <realhenryiyke@gmail.com>
2021-07-12 12:20:35 +0100

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Entry point
+ * @argc: Argument count
+ * @argv: Array of Arguments
+ *
+ * Return: Return 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
