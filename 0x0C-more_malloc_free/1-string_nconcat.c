18168e18da365b16a3405639793a8f2c902e44cf
HenryIyke <realhenryiyke@gmail.com>
2021-07-15 16:13:47 +0100

    Second commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,27 +11,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  int i, j, len1 = 0;
-  unsigned int len2 = 0;
-  char *s3;
+int i, j, len1 = 0;
+unsigned int len2 = 0;
+char *s3;
 
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
-  for (i = 0; s1[i] != '\0'; i++)
-    len1++;
-  for (j = 0; s2[j] != '\0'; j++)
-    len2++;
-  s3 = malloc((len1 + (n + 1)) * sizeof(char));
-  if (s3 == NULL)
-    return (NULL);
-  if (n >= len2)
-    n = len2;
-  for (i = 0; s1[i] != '\0'; i++)
-    s3[i] = s1[i];
-  for (j = 0; s2[j] != '\0'; j++, i++)
-    s3[i] = s2[j];
-  s3[i] = '\0';
-  return (s3);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+len1++;
+for (j = 0; s2[j] != '\0'; j++)
+len2++;
+if (n >= len2)
+n = len2;
+s3 = malloc((len1 + ((n * (sizeof(*s2))) + 1) * (sizeof(*s3)));
+if (s3 == NULL)
+return (NULL);
+for (i = 0; s1[i] != '\0'; i++)
+s3[i] = s1[i];
+for (j = 0; s2[j] != '\0'; j++, i++)
+s3[i] = s2[j];
+s3[i] = '\0';
+return (s3);
 }
