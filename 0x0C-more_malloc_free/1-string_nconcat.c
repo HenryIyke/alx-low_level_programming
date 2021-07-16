b1fb992b5c5cb3d7f21ad8379cc7652b0015a8d4
HenryIyke <realhenryiyke@gmail.com>
2021-07-16 18:01:48 +0100

    Third commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,22 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, len1 = n;
-unsigned int len2 = 0;
-char *s3;
+char *str, *str_tmp;
+unsigned int i = 0, len_1 = 0, len_2 = 0;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (i = 0; s1[i] != '\0'; i++)
-len1++;
-for (j = 0; s2[j] != '\0'; j++)
-len2++;
-s3 = malloc(sizeof(char) * (len1 + 1));
-if (s3 == NULL)
+s1 = s1 == NULL ? "" : s1, s2 = s2 == NULL ? "" : s2;
+for (; s1[len_1]; ++len_1)
+continue;
+for (; s2[len_2] && len_2 < n; ++len_2)
+continue;
+
+str = malloc(sizeof(char) * (len_1 + len_2 + 1)), str_tmp = str;
+if (str == NULL)
 return (NULL);
-len1 = 0;
-for (i = 0; s1[i]; i++)
-s3[len1++] = s1[i];
-for (j = 0; s2[j] && i < n; j++)
-s3[len1++] = s2[j];
-s3[len1] = '\0';
-return (s3);
+while (*s1)
+*str++ = *s1++;
+for (; i < len_2; i++)
+*str++ = *s2++;
+*str = '\0';
+return (str_tmp);
 }
