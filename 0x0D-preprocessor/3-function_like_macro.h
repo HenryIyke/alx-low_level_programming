aef2da50b3eb65c996298004b93626d7245c4915
HenryIyke <realhenryiyke@gmail.com>
2021-07-19 18:06:59 +0100

    Third commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
+#ifndef ABS_HEADER
+#define ABS_HEADER
+
 #define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,6 @@
-#ifndef SUM
+#ifndef SUM_HEADER
+#define SUM_HEADER
+
 #define SUM(x, y) ((x) + (y))
+
 #endif
