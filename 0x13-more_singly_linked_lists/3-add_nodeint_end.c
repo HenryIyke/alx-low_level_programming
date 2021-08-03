941ea0da18d74bc34bf6f15bbce63f8a19da4ee1
HenryIyke <realhenryiyke@gmail.com>
2021-08-03 15:52:27 +0100

    First commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
-	listint_t *new_node;
+	listint_t *tail;
 
 	tail = head ? *head : NULL;
 	while (tail && tail->next != NULL)
