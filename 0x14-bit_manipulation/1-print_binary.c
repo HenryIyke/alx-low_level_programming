1c7d6fc88fbea0930ebb1d7e896b696a3c4822df
HenryIyke <realhenryiyke@gmail.com>
2021-08-05 21:44:49 +0100

    Third commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * print_binary - Prints the binary representation of a number
@@ -8,28 +9,12 @@
 void print_binary(unsigned long int n)
 {
 	if (n < 2)
+	{
 		_putchar(n + '0');
+	}
 	else
 	{
 		print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * on error, -1 is returned, and errno is set appropriately
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
