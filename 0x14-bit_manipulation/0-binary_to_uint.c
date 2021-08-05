e070754a3ddab3cf8bc1384a47aeb5ba5557ab0a
HenryIyke <realhenryiyke@gmail.com>
2021-08-05 16:06:57 +0100

    Second commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
-#include <math.h>
 #include <stdio.h>
 #include "main.h"
+#include <math.h>
 
 /**
  * binary_to_uint - Converts binary to an unsigned int
@@ -13,34 +13,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int bi = 0, j = 0, len = 0;
+	unsigned int bi = 0, len = 0;
 	int i;
 
-	for (i = 0; b[i] != '\0'; i++)
-		len++;
-	for (i = len; i >= 0; i--,j++)
-	{
-		bi += (b[i] * (_pow(10, j)));
+	if (b == NULL)
+		return (0);
 
-		if ((b[i] != '0' || b[i] != '1') && b == NULL)
+	for (i = 0; b[i] != '\0'; i++, len++)
+	{
+		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
 	}
+
+	for (i = 0; b[i] != '\0'; i++)
+		bi += ((b[i] - '0') * (1 << (len - i -1)));
+
 	return (bi);
-}
-
-/**
- * _pow - Returns a power
- * @x: integer argument
- * @y: integer power
- *
- * Return: value of X raised to the power y.
- */
-int _pow(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	if (y < 0)
-		return (-1);
-
-	return (x * _pow(x, y - 1));
 }
