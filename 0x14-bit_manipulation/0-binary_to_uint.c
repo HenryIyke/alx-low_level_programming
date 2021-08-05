efb78996976416837039a75e30cdaf4839bf9889
HenryIyke <realhenryiyke@gmail.com>
2021-08-05 16:22:18 +0100

    Second commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,15 +13,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int bi = 0, len = 0;
-	int i;
+	unsigned int bi = 0;
+	int i, len;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++, len++)
+	for (len = 0; b[len] != '\0'; len++)
 	{
-		if (!(b[i] == '0' || b[i] == '1'))
+		if (!(b[len] == '0' || b[len] == '1'))
 			return (0);
 	}
 
