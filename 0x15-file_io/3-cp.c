1705c1b86b4650044e07c1c1664934bb15e96a31
HenryIyke <realhenryiyke@gmail.com>
2021-08-09 16:01:39 +0100

    Second commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define MAXSIZE 1204
+#define SE STDERR_FILENO
+
+
+void file_copy(int src_fd, int dest_fd, char *dest_name);
+void close_fd(int fd);
+
+
+/**
+ * main - creates the copy bash script
+ * @argc: argument count
+ * @argv: argument string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int src_fd, dest_fd;
+	mode_t mode;
+
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	if (argc != 3)
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
+		dprintf(SE, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (dest_fd == -1)
+		dprintf(SE, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	file_copy(src_fd, dest_fd, argv[2]);
+	close_fd(src_fd), close_fd(dest_fd);
+	return (0);
+}
+
+/**
+ * file_copy - Copies the content from one file to another
+ * @src_fd: The source file
+ * @dest_fd: The destination file
+ * @dest_name: Destination filename
+ */
+void file_copy(int src_fd, int dest_fd, char *dest_name)
+{
+	int i, c;
+	void *buf = malloc(sizeof(char) * MAXSIZE);
+
+	if (!buf)
+	{
+		for (i = 0; ; i += MAXSIZE)
+		{
+			c = read(src_fd, buf, MAXSIZE);
+			if (c == 0)
+				break;
+			if (write(dest_fd, buf, c) != c)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_name);
+				exit(99);
+			}
+		}
+		free(buf);
+	}
+}
+
+
+/**
+ * close_fd - closes the file
+ * @fd: open file
+ */
+
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+}
