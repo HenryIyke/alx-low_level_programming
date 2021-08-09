8dc7cd7db6934fac993ba5acb819922bb0b4cf73
HenryIyke <realhenryiyke@gmail.com>
2021-08-09 16:24:25 +0100

    Second commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,10 +29,17 @@ int main(int argc, char *argv[])
 		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
 	src_fd = open(argv[1], O_RDONLY);
 	if (src_fd == -1)
+	{
+		close_fd(src_fd);
 		dprintf(SE, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (dest_fd == -1)
-		dprintf(SE, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	dest_fd = open(argv[2], O_WRONLY | O_TRUNC);
+	if (dest_fd < 0)
+	{
+		dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		if (dest_fd < 0)
+			dprintf(SE, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 
 	file_copy(src_fd, dest_fd, argv[2]);
 	close_fd(src_fd), close_fd(dest_fd);
