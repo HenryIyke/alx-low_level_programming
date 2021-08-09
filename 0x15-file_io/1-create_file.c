1b190bc29cd78db11bccfc9b79c03d31c8d8c057
HenryIyke <realhenryiyke@gmail.com>
2021-08-09 13:26:02 +0100

    First commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of file to be created
+ * @text_content: Content of the file
+ *
+ * Return: 1 0n success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, rcheck, x;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		rcheck = write(fd, text_content, i);
+		if (rcheck == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
